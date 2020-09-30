fe1f474dffa3ca89a6165593a13f13f50108b9cb
shuheiktgw <shuheiktgw@users.noreply.github.com>
2020-09-30 17:17:04 +0900

    Bypass Capture plugin and print out messages

--- launchable/plugin.py
+++ launchable/plugin.py
@@ -1,7 +1,9 @@
 
+import sys
 import traceback
 
 from nose.plugins import Plugin
+from nose.plugins.capture import Capture
 
 from launchable.client import LaunchableClientFactory
 from launchable.log import logger
@@ -10,6 +12,12 @@ from launchable.manager import parse_test, reorder
 
 class Launchable(Plugin):
     name = "launchable"
+    # Grab stdout before the capture plugin
+    score = Capture.score + 1
+
+    def __init__(self):
+        super().__init__()
+        self._stdout = []
 
     def options(self, parser, env):
         super(Launchable, self).options(parser, env=env)
@@ -19,18 +27,27 @@ class Launchable(Plugin):
         super(Launchable, self).configure(options, conf)
         self.enabled = options.launchable
 
+    def begin(self):
+        self._stdout.append(sys.stdout)
+
     def prepareTest(self, test):
         try:
             t = parse_test(test)
 
             launchable_client = LaunchableClientFactory.prepare()
+            self._print("Getting optimized test execution order from Launchable...")
             order = launchable_client.infer(t)
+            self._print("Received optimized test execution order from Launchable")
 
             reorder(test, order)
 
-            print("Launchable has successfully reordered tests")
+            self._print("Test execution optimized by Launchable 🚀")
             return
         except Exception as error:
             logger.warning("An exception occurred while reordering. Executing tests in standard order: {}".format(error))
             logger.warning(traceback.format_exc())
             return test
+
+    # Bypass Capture plugin
+    def _print(self, message):
+        self._stdout[0].write(message + "\n")
