107ef5a20b3d2335c5505b2550eb90775b0b426b
shuheiktgw <shuheiktgw@users.noreply.github.com>
2020-12-01 16:20:23 +0900

    Fix the deploy scripts

--- scripts/release_production.sh
+++ scripts/release_production.sh
@@ -1,14 +1,14 @@
-current=`cat launchable/version.py`
+current=`cat nose_launchable/version.py`
 
 read -p "Next version (current version: $current): " next
-echo "__version__ = '$next'" > launchable/version.py
+echo "__version__ = '$next'" > nose_launchable/version.py
 
 ./scripts/build.sh
 
 git commit -am "Bumps up to v$next"
 git tag "v$next"
 
-git push origin main
-git push origin --tags
+git push upstream main
+git push upstream --tags
 
 twine upload --repository pypi dist/*
--- scripts/release_test.sh
+++ scripts/release_test.sh
@@ -1,7 +1,7 @@
-current=`cat launchable/version.py`
+current=`cat nose_launchable/version.py`
 
 read -p "Next version (current version: $current): " next
-echo "__version__ = '$next'" > launchable/version.py
+echo "__version__ = '$next'" > nose_launchable/version.py
 
 ./scripts/build.sh
 
