d51b114e2fbfb78cd7c02ddd87819b50a0d72f94
shuheiktgw <shuheiktgw@users.noreply.github.com>
2021-07-15 14:53:41 +0900

    Fix manager tests

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -4,13 +4,15 @@ from .resources.module1 import MockTestClass1
 from .resources.module2 import MockTestClass2
 
 from nose.suite import ContextSuite
+from nose.suite import Test
 from nose_launchable.manager import get_test_names, subset
 
 
 class TestManager(unittest.TestCase):
     def setUp(self):
-        self.mock_suite1 = ContextSuite(tests=[ContextSuite(context=MockTestClass1), ContextSuite(context=MockTestClass2)])
-        self.mock_suite0 = ContextSuite(tests=[ContextSuite(context=MockTestClass0), self.mock_suite1])
+        func = lambda: print("called")
+        self.mock_suite1 = ContextSuite(tests=[ContextSuite(tests=[Test(func)], context=MockTestClass1), ContextSuite(tests=[Test(func)], context=MockTestClass2)])
+        self.mock_suite0 = ContextSuite(tests=[ContextSuite(tests=[Test(func)], context=MockTestClass0), self.mock_suite1])
 
     def test_get_test_names(self):
         want = ['tests/resources/module0.py', 'tests/resources/module1.py', 'tests/resources/module2.py']
