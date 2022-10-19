9ad175fb97f98d62ff4059e3bafd033c6bc06f56
Konboi <ryosuke.yabuki@gmail.com>
2022-10-19 16:53:48 +0900

    unclear envrionment value

--- tests/test_protecter.py
+++ tests/test_protecter.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from nose_launchable.protecter import protect
+from nose_launchable.protecter import REPORT_ERROR_KEY, protect
 
 
 class TestProtector(unittest.TestCase):
@@ -17,6 +17,12 @@ class TestProtector(unittest.TestCase):
         def error_func():
             raise Exception
 
-        os.environ["LAUNCHABLE_REPORT_ERROR"] = '1'
+        current_config = os.getenv(REPORT_ERROR_KEY, "")
+        os.environ[REPORT_ERROR_KEY] = '1'
         with self.assertRaises(Exception):
             error_func()
+
+        if current_config:
+            os.environ[REPORT_ERROR_KEY] = current_config
+        else:
+            os.environ.pop(REPORT_ERROR_KEY)
